a351b0ba06a549b8a819a3db26ef3a8736a9f6e2
admin admin <evaladmin@example.com>
2020-06-23 15:25:50 +0700

    User admin wrote content /scripts/components/news.groovy

--- scripts/components/news.groovy
+++ scripts/components/news.groovy
@@ -1,6 +1,4 @@
 import org.craftercms.blueprints.headless.NewsSearchHelper
-
-
 def searchHelper = new NewsSearchHelper(elasticsearch, urlTransformationService)
 def news = searchHelper.searchNews(null)
 println "News:"
