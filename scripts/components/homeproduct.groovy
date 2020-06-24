4ab59c74e8ad4f4b912fc4333906fd277865c501
admin admin <evaladmin@example.com>
2020-06-24 07:27:15 +0000

    User admin wrote content /scripts/components/homeproduct.groovy

--- scripts/components/homeproduct.groovy
+++ scripts/components/homeproduct.groovy
@@ -1,6 +1,6 @@
 import org.craftercms.blueprints.headless.GroupProductSearchHelper
 
 def searchHelper = new GroupProductSearchHelper(elasticsearch, urlTransformationService)
-def products = searchHelper.searchProducts(null,0)
+def products = searchHelper.searchProducts(null,0,5)
 
 templateModel.products = products
