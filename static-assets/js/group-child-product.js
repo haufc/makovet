31c32392cefe1ab89f738aae404e052b2a9e21b4
admin admin <evaladmin@example.com>
2020-06-24 04:40:22 +0000

    User admin wrote content /static-assets/js/group-child-product.js

--- static-assets/js/group-child-product.js
+++ static-assets/js/group-child-product.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     
     var totalPages = Math.round(numberOfProducts / limitPerPage );
     
-    $('.pagi').append("<li style='background-color: #322372 !important' class='page-item current-page active'><a class='page-link' href='javacript:void(0)'>"+ 1+"</a></li>");
+    $('.pagi').append("<li class='page-item current-page active'><a style='background-color: #322372 !important' class='page-link' href='javacript:void(0)'>"+ 1+"</a></li>");
     for (let i=2; i<= totalPages;i++){
         $(".pagi").append("<li class='page-item current-page'><a class='page-link' href='javascript:void(0)'>"+ i +"</a></li>");
     }
