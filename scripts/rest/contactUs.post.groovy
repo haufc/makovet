5d7e008aaade6e5509c242bf13e8b5056f76e7b6
admin admin <evaladmin@example.com>
2020-06-25 07:29:41 +0000

    User admin wrote content /scripts/rest/contactUs.post.groovy

--- scripts/rest/contactUs.post.groovy
+++ scripts/rest/contactUs.post.groovy
@@ -10,7 +10,7 @@ service.setPort('587')
 service.setEncoding('utf-8')
 
 service.sendEmail(params.email, "keysoft.hotro@gmail.com", "hoandg@keysoft.vn", 
-                  params.title ,"Họ Tên: " + params.name + "Số điện thoại: " + params.phone + " Tên công ty: " + params.company + " Nội dung: " + params.content)
+                  params.title ,"Họ Tên: " + params.name + "<br/>" + "Số điện thoại: " + params.phone + "<br/>" + " Tên công ty: " + params.company + "<br/>" + " Nội dung: " + params.content)
 
 def result=[:]
 result.success = true
