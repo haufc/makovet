6841fcd5b023168383392296039bdd70d306a199
admin admin <evaladmin@example.com>
2020-06-25 07:11:04 +0000

    User admin wrote content /scripts/rest/contactUs.post.groovy

--- scripts/rest/contactUs.post.groovy
+++ scripts/rest/contactUs.post.groovy
@@ -7,6 +7,7 @@ def service = new MailService()
 // SMTP server information:
 service.setHost('smtp.gmail.com')
 service.setPort('587')
+service.setEncoding('utf-8')
 
 service.sendEmail(params.email, "keysoft.hotro@gmail.com", "hoandg@keysoft.vn", 
                   params.title ,"Họ Tên: " + params.name + "Số điện thoại: " + params.phone + " Tên công ty: " + params.company + " Nội dung: " + params.content)
