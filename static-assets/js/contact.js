c3676ea95add1da269dcbe9abb549b8908e9a27a
admin admin <evaladmin@example.com>
2020-06-25 07:55:03 +0000

    User admin wrote content /static-assets/js/contact.js

--- static-assets/js/contact.js
+++ static-assets/js/contact.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
       if (!this.checkValidity()) {
           // If the form is invalid, submit it. The form won't actually submit;
           // this will just cause the browser to display the native HTML5 error messages.
-          this.find('button[type="submit"]').click()
+          this.find('button[type="submit"]').click();
+          
       } else {
         var values = $(this).serialize();
         $.ajax({
@@ -14,6 +15,7 @@ $(document).ready(function() {
           data: values,
           success: function(data){
               if (data.success) {
+                  $('#modal-spinner').modal('hide');
                   $('#success-dialog').modal('show');
                   $('form')[0].reset();
               } else {
@@ -21,9 +23,17 @@ $(document).ready(function() {
               }
           },
           error: function(response) {
+              $('#modal-spinner').modal('hide');
               $('#fail-dialog').modal('show');
           }
         });
       }
   });
 });
+
+function openSpinner() {
+     $('#modal-spinner').modal('show');
+        setTimeout(function () {
+       	$('.modal').modal('hide');
+    }, 3000);
+}
