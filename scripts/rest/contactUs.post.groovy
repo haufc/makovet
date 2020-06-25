3eafd636b501c32d81c9f569b3b2d4037faac564
admin admin <evaladmin@example.com>
2020-06-25 03:28:29 +0000

    User admin wrote content /scripts/rest/contactUs.post.groovy

--- scripts/rest/contactUs.post.groovy
+++ scripts/rest/contactUs.post.groovy
@@ -1,5 +1,12 @@
+import org.craftercms.blueprints.services.MailService
+
 println "Processing Contact Us Request with values:"
 println params
-def result=[:]
-result.success = true
-return result
+
+def service = new MailService()
+// SMTP server information:
+service.host= smtp.gmail.com
+service.port= 587
+
+service.sendEmail(params.email, "haufc1997@gmail.com", "anhnt@keysoft.vn", params.title ,
+                  "/templates/mail/mail.ftl",[data:"modle", "of":"theTemplate"])
