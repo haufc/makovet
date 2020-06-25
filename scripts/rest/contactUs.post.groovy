de0def01a7566e5b9003aa80362232bedee28e9e
admin admin <evaladmin@example.com>
2020-06-25 03:47:04 +0000

    User admin wrote content /scripts/rest/contactUs.post.groovy

--- scripts/rest/contactUs.post.groovy
+++ scripts/rest/contactUs.post.groovy
@@ -5,7 +5,7 @@ println params
 
 def service = new MailService()
 // SMTP server information:
-service.setHost(smtp.gmail.com)
+service.setHost('smtp.gmail.com')
 service.setPort(587)
 
 service.sendEmail(params.email, "haufc1997@gmail.com", "anhnt@keysoft.vn", params.title ,
