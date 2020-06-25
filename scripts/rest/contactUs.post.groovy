ceb784f3f8c6de5c703b8191ea4df4bdb1b62fee
admin admin <evaladmin@example.com>
2020-06-25 03:46:30 +0000

    User admin wrote content /scripts/rest/contactUs.post.groovy

--- scripts/rest/contactUs.post.groovy
+++ scripts/rest/contactUs.post.groovy
@@ -5,8 +5,8 @@ println params
 
 def service = new MailService()
 // SMTP server information:
-service.host= smtp.gmail.com
-service.port= 587
+service.setHost(smtp.gmail.com)
+service.setPort(587)
 
 service.sendEmail(params.email, "haufc1997@gmail.com", "anhnt@keysoft.vn", params.title ,
                   "/templates/mail/mail.ftl",[data:"modle", "of":"theTemplate"])
