c839c5c5c3e41a4bc659d5f459240abf5fc9fe5a
admin admin <evaladmin@example.com>
2020-06-26 07:52:20 +0000

    User admin wrote content /static-assets/js/techsupport.js

---
+++ static-assets/js/techsupport.js
@@ -0,0 +1,86 @@
+ $(document).ready(function(){
+      // Panigation
+    var groupProductSize = $('#group-pr-length').val();
+    var limitperPage = 3;
+    
+    for(let i = 0; i < groupProductSize; i++) {
+        var numberOfGroupItem = $('.group-pr-'+i).find('.list-tech-sp-vn .item-pr a').length;
+        var groupItem = $('.group-pr-'+i).find('.list-tech-sp-vn .item-pr a');
+        var groupItemDiv = $('.group-pr-'+i).find('.list-tech-sp-vn .item-pr');
+        $('.group-pr-'+i+' .paginate ul').attr('id', 'pagi-'+i);
+        $('.group-pr-'+i+' .paginate ul').find('li').attr('id', 'prevous-item-'+i);
+        
+        // hiden element over limitperPage
+        if (numberOfGroupItem > limitperPage) {
+            for(let j = limitperPage; j <  numberOfGroupItem; j++) {
+                $(groupItemDiv[j]).hide();
+            }
+            
+            var totalPages;
+            if ((numberOfGroupItem / limitperPage) % 2 == 0) {
+               totalPages =  Math.round(numberOfGroupItem / limitperPage );
+            } else {
+                totalPages =  Math.round(numberOfGroupItem / limitperPage ) + 1;
+            }
+            
+            $('.group-pr-' + i + ' .paginate #pagi-'+i).append("<li class='page-item current-page active'><a class='page-link' href='javacript:void(0)'>"+ 1+"</a></li>");
+            
+            for (let j=2; j<= totalPages;j++){
+                 $('.group-pr-' + i + ' .paginate #pagi-'+i).append("<li class='page-item current-page'><a class='page-link' href='javascript:void(0)'>"+ j +"</a></li>");
+            }   
+            
+            var strId = "next-page-"+i;
+            $('.group-pr-' + i + ' .paginate #pagi-'+i).append("<li id='"+strId+"' class='page-item'><a class='page-link' href='javascript:void(0)'><span class='fas fa-angle-right'></a></li>");
+        
+            $('.group-pr-' + i + ' .paginate #pagi-'+ i +' li.current-page').on("click", function(){
+                if($(this).hasClass("active")){
+                    return false;
+                } else{
+                    var currentPage = $(this).index();
+                    $('.group-pr-' + i + ' .paginate #pagi-'+ i +' li').removeClass("active");
+                    $(this).addClass("active");
+                    $('.group-pr-'+i+' .list-tech-sp-vn .item-pr').hide();
+                    var total = limitperPage * currentPage;
+                    for(let z = total - limitperPage; z<total; z++){
+                        $('.group-pr-'+ i +' .list-tech-sp-vn .item-pr').eq(z).show();
+                    }
+                }
+            });
+            
+            $("#next-page-"+i).on("click", function() {
+              var currentPage = $('.group-pr-' + i + ' .paginate #pagi-'+ i +' li.active').index(); 
+              if (currentPage === totalPages) {
+                return false; 
+              } else {
+                currentPage++; 
+                $('.group-pr-' + i + ' .paginate #pagi-'+ i +' li').removeClass("active");
+                $('.group-pr-'+i+' .list-tech-sp-vn .item-pr').hide();
+                var total = limitperPage * currentPage; 
+                for(let z = total - limitperPage; z<total; z++){
+                    $('.group-pr-'+ i +' .list-tech-sp-vn .item-pr').eq(z).show();
+                }
+                $('.group-pr-' + i + ' .paginate #pagi-'+ i +' li.current-page:eq('+ (currentPage - 1)+')').addClass('active');
+              }
+            });
+            
+             $('#prevous-item-'+i).on("click", function() {
+                  var currentPage = $('.group-pr-' + i + ' .paginate #pagi-'+ i +' li.active').index(); 
+                  if (currentPage === 1) {
+                    return false; 
+                  } else {
+                    currentPage--; 
+                    $('.group-pr-' + i + ' .paginate #pagi-'+ i +' li').removeClass("active");
+                    $('.group-pr-'+i+ ' .list-tech-sp-vn .item-pr').hide();
+                    var grandTotal = limitperPage * currentPage; 
+                    for (let z = grandTotal - limitperPage; z < grandTotal; z++) {
+                       $('.group-pr-'+ i +' .list-tech-sp-vn .item-pr').eq(z).show();
+                    }
+                    $('.group-pr-' + i + ' .paginate #pagi-'+ i +' li.current-page:eq('+ (currentPage - 1)+')').addClass('active');
+                  }
+                });
+        } else {
+            $('.group-pr-' + i + ' .paginate #pagi-'+ i).hide();
+        }
+        
+    }
+ });
