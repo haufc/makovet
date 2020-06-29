45b7c38ceb5772d08c0a14defac3475fe2b37eb2
admin admin <evaladmin@example.com>
2020-06-29 09:57:06 +0700

    User admin wrote content /scripts/components/newsinhome.groovy

--- scripts/components/newsinhome.groovy
+++ scripts/components/newsinhome.groovy
@@ -1,15 +1,10 @@
 import org.craftercms.blueprints.headless.NewsSearchHelper
 def searchHelper = new NewsSearchHelper(elasticsearch, urlTransformationService)
 def news = searchHelper.searchNews(contentModel.newscategory_s, 0)
-println "News:"
-print news
+
 def firstNew = news[0]
 def lastNew = news[news.size()-1]
 
-println "first"
-println firstNew
 
-println "last"
-print lastNew
-
-templateModel.news = news
+templateModel.firstNew = firstNew
+templateModel.lastNew = lastNew
