27ca1a9324c96f6d9b1945b5b6f42fe4f8aff1e6
admin admin <evaladmin@example.com>
2020-06-29 09:55:03 +0700

    User admin wrote content /scripts/components/newsinhome.groovy

--- scripts/components/newsinhome.groovy
+++ scripts/components/newsinhome.groovy
@@ -3,5 +3,13 @@ def searchHelper = new NewsSearchHelper(elasticsearch, urlTransformationService)
 def news = searchHelper.searchNews(contentModel.newscategory_s, 0)
 println "News:"
 print news
+def firstNew = news[0]
+def lastNew = news[news.size()-1]
+
+println "first"
+println firstNew
+
+println "last"
+print lastNew
 
 templateModel.news = news
