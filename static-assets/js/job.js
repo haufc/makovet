fff3269644ffe54595a1b1c068b2c3653a5625c9
admin admin <evaladmin@example.com>
2020-06-29 08:10:21 +0000

    User admin wrote content /static-assets/js/job.js

--- static-assets/js/job.js
+++ static-assets/js/job.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     $("#panigation-job .job-item:gt(" + (limitPerPage -1 )+")").hide();
     
     var totalPages;
-    if ((numberOfProducts / limitperPage) % 2 == 0) {
+    if ((numberOfProducts / limitPerPage) % 2 == 0) {
        totalPages =  Math.round(numberOfProducts / limitPerPage );
     } else {
         totalPages =  Math.round(numberOfProducts / limitPerPage ) + 1;
