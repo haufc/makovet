3c4de9b01e5f5f3bf184807e713fb97c55b4597b
admin admin <evaladmin@example.com>
2020-07-01 14:27:08 +0700

    User admin wrote content /static-assets/js/contact-58545a7e.js

--- static-assets/js/contact-58545a7e.js
+++ static-assets/js/contact-58545a7e.js
@@ -1,39 +1,128 @@
-$(document).ready(function() {
-    $('form').submit(function(event) {
-      event.preventDefault()
-
-      if (!this.checkValidity()) {
-          // If the form is invalid, submit it. The form won't actually submit;
-          // this will just cause the browser to display the native HTML5 error messages.
-          this.find('button[type="submit"]').click();
-          
+    var numberOfFieldsNews = $("#field-news .fields").length;
+    var numberOfMavinexNews = $("#mavinex-news .mavinex").length;
+    
+    var limitPerPage = 2;
+    
+    $("#mavinex-news .mavinex:gt(" + (limitPerPage -1 )+")").hide();
+    $("#field-news .fields:gt(" + (limitPerPage -1 )+")").hide();
+    
+    var totalPages1 = Math.round(numberOfMavinexNews / limitPerPage );
+    var totalPages2 = Math.round(numberOfFieldsNews / limitPerPage );
+    
+    $('.pagi1').append("<li class='page-item current-page active'><a class='page-link' href='javacript:void(0)'>"+ 1+"</a></li>");
+    $('.pagi2').append("<li class='page-item current-page active'><a class='page-link' href='javacript:void(0)'>"+ 1+"</a></li>");
+    
+    for (let i=2; i<= totalPages1;i++){
+        $(".pagi1").append("<li class='page-item current-page'><a class='page-link' href='javascript:void(0)'>"+ i +"</a></li>");
+    }
+    
+    for (let i=2; i<= totalPages2;i++){
+        $(".pagi2").append("<li class='page-item current-page'><a class='page-link' href='javascript:void(0)'>"+ i +"</a></li>");
+    }
+    
+    $('.pagi1').append("<li id='next-page1' class='page-item'><a class='page-link' href='javascript:void(0)'><span class='fas fa-angle-right'></a></li>");
+    $('.pagi2').append("<li id='next-page2' class='page-item'><a class='page-link' href='javascript:void(0)'><span class='fas fa-angle-right'></a></li>");
+    
+    
+    $('.pagi1 li.current-page').on("click", function(){
+        if($(this).hasClass("active")){
+            return false;
+        } else{
+            var currentPage = $(this).index();
+            $('.pagi1 li').removeClass("active");
+            $(this).addClass("active");
+            $("#mavinex-news .mavinex").hide();
+            var total = limitPerPage * currentPage;
+            for(let i = total - limitPerPage; i<total; i++){
+                $("#mavinex-news .mavinex:eq("+ i +")").show();
+            }
+        }
+    });
+    
+    $("#next-page1").on("click", function() {
+      var currentPage = $(".pagination li.active").index(); 
+      if (currentPage === totalPages1) {
+        return false; 
       } else {
-        var values = $(this).serialize();
-        $.ajax({
-          type: this.method,
-          url: this.action,
-          data: values,
-          success: function(data){
-              if (data.success) {
-                  $('#modal-spinner').modal('hide');
-                  $('#success-dialog').modal('show');
-                  $('form')[0].reset();
-              } else {
-                  $('#fail-dialog').modal('show');
-              }
-          },
-          error: function(response) {
-              $('#modal-spinner').modal('hide');
-              $('#fail-dialog').modal('show');
+        currentPage++; 
+        $(".pagi1 li").removeClass('active'); 
+        $("#mavinex-news .mavinex").hide();
+        var total = limitPerPage * currentPage; 
+        for (let i = total - limitPerPage; i < total; i++) {
+          $("#mavinex-news .mavinex:eq(" + i + ")").show(); 
+        }
+    
+        $(".pagi1 li.current-page:eq(" + (currentPage -1) + ")").addClass('active'); 
+      }
+    });
+
+
+    $("#previous-page1").on("click", function() {
+          var currentPage = $(".pagi1 li.active").index(); 
+          if (currentPage === 1) {
+            return false; 
+          } else {
+            currentPage--; 
+            $(".pagi1 li").removeClass('active'); 
+            $("#mavinex-news .mavinex").hide();
+            var grandTotal = limitPerPage * currentPage; 
+            for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
+              $("#mavinex-news .mavinex:eq(" + i + ")").show();
+            }
+            $(".pagi1 li.current-page:eq(" + (currentPage - 1) + ")").addClass('active'); 
           }
         });
+        
+    $('.pagi2 li.current-page').on("click", function(){
+        if($(this).hasClass("active")){
+            return false;
+        } else{
+            var currentPage = $(this).index();
+            $('.pagi2 li').removeClass("active");
+            $(this).addClass("active");
+            $("#field-news .fields").hide();
+            var total = limitPerPage * currentPage;
+            for(let i = total - limitPerPage; i<total; i++){
+                $("#field-news .fields:eq("+ i +")").show();
+            }
+        }
+    });
+    
+    $("#next-page2").on("click", function() {
+      var currentPage = $(".pagi2 li.active").index(); 
+      if (currentPage === totalPages2) {
+        return false; 
+      } else {
+        currentPage++; 
+        $(".pagi2 li").removeClass('active'); 
+        $("#field-news .fields").hide();
+        var total = limitPerPage * currentPage; 
+        for (let i = total - limitPerPage; i < total; i++) {
+          $("#field-news .fields:eq(" + i + ")").show(); 
+        }
+    
+        $(".pagi2 li.current-page:eq(" + (currentPage -1) + ")").addClass('active'); 
       }
-  });
-});
+    });
 
-function openSpinner() {
-     $('#modal-spinner').modal('show');
-        setTimeout(function () {
-       	$('.modal').modal('hide');
-    }, 4000);
-}
+
+    $("#previous-page2").on("click", function() {
+          var currentPage = $(".pagi2 li.active").index(); 
+          if (currentPage === 1) {
+            return false; 
+          } else {
+            currentPage--; 
+            $(".pagi2 li").removeClass('active'); 
+            $("#field-news .fields").hide();
+            var grandTotal = limitPerPage * currentPage; 
+            for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
+              $("#field-news .fields:eq(" + i + ")").show();
+            }
+            $(".pagi2 li.current-page:eq(" + (currentPage - 1) + ")").addClass('active'); 
+          }
+        });    
+    
+    $('.limit-text').each(function (f) {
+    var newstr = $(this).text().substring(0,300);
+    $(this).text(newstr).append("...");
+    });
