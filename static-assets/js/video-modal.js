69520a349fb646478ffa9805f8a979316af4069b
admin admin <evaladmin@example.com>
2020-07-06 16:20:47 +0700

    User admin wrote content /static-assets/js/video-modal.js

--- static-assets/js/video-modal.js
+++ static-assets/js/video-modal.js
@@ -9,7 +9,7 @@ $('#video-1').click(function() {
 });
 
 // when the modal is opened autoplay it  
-$('#myModal').on('shown.bs.modal', function (e) {
+$('#videoModal').on('shown.bs.modal', function (e) {
     
 // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
 $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
@@ -18,7 +18,7 @@ $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinf
 
 
 // stop playing the youtube video when I close the modal
-$('#myModal').on('hide.bs.modal', function (e) {
+$('#videoModal').on('hide.bs.modal', function (e) {
     // a poor man's stop video
     $("#video").attr('src',$videoSrc); 
 }) 
