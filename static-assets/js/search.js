724685454535b7d268de071bd2f04a3da98dd609
admin admin <evaladmin@example.com>
2020-07-08 10:47:04 +0700

    User admin wrote content /static-assets/js/search.js

--- static-assets/js/search.js
+++ static-assets/js/search.js
@@ -1,8 +1,8 @@
 function toggleSearchBar() {
-    var nav = document.getElementById("searchBar");
-    if(nav.className === "search-bar") {
-        nav.className += " active";
+    var search = document.getElementById("searchBar");
+    if(search.className === "search-bar") {
+        search.className += " active";
     } else {
-        nav.className = "search-bar";
+        search.className = "search-bar";
     }
 }
