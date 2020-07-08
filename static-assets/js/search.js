8b2dc5961b3a8ae29bfd14ba0884a740380cb03c
admin admin <evaladmin@example.com>
2020-07-08 10:37:20 +0700

    User admin wrote content /static-assets/js/search.js

--- static-assets/js/search.js
+++ static-assets/js/search.js
@@ -1,8 +1,8 @@
 function toggleSearchBar() {
     var nav = document.getElementById("searchBar");
-    if(nav.className === "mavinex-nav__search-bar") {
+    if(nav.className === "search-bar") {
         nav.className += " active";
     } else {
-        nav.className = "mavinex-nav__search-bar";
+        nav.className = "search-bar";
     }
 }
