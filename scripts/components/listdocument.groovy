214e4f64662201bd29cdf1ecdf80ef87cac6557b
admin admin <evaladmin@example.com>
2020-07-09 15:11:00 +0700

    User admin wrote content /scripts/components/listdocument.groovy

--- scripts/components/listdocument.groovy
+++ scripts/components/listdocument.groovy
@@ -1,6 +1,11 @@
 import org.craftercms.blueprints.headless.DocumentSearchHelper
+import org.craftercms.blueprints.headless.DocumentPDFSearchHelper
 
 def searchHelper = new DocumentSearchHelper(elasticsearch, urlTransformationService)
+def searchPDFHelper = new DocumentPDFSearchHelper(elasticsearch, urlTransformationService)
+
 def docs = searchHelper.searchdocs(0)
+def docsPDF = searchPDFHelper.searchdocs(0)
 
 templateModel.docs = docs
+templateModel.docsPDF = docsPDF
