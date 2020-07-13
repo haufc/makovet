1b660276615f8441330ddfae0ca5582d009ca440
admin admin <evaladmin@example.com>
2020-07-13 17:10:30 +0700

    User admin wrote content /scripts/components/ennewsinhome.groovy

--- scripts/components/ennewsinhome.groovy
+++ scripts/components/ennewsinhome.groovy
@@ -1,4 +1,4 @@
-import org.craftercms.blueprints.headless.NewsSearchHelper
+import org.craftercms.blueprints.headless.NewsEnSearchHelper
 def searchHelper = new NewsEnSearchHelper(elasticsearch, urlTransformationService)
 def news = searchHelper.searchNewsEn(contentModel.newscategory_s, 0)
 
