d210c71a6926ed69ef69cb8ef8c6987f61d85c49
admin admin <evaladmin@example.com>
2020-07-13 14:04:43 +0700

    Delete file /scripts/components/entinntuc.groovy

--- scripts/components/entinntuc.groovy
+++
@@ -1,7 +0,0 @@
-import org.craftercms.blueprints.headless.NewsEnSearchHelper
-def searchHelper = new NewsEnSearchHelper(elasticsearch, urlTransformationService)
-def news = searchHelper.searchNewsEn(null)
-println "News:"
-print news
-
-templateModel.news = news
