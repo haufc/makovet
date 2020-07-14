6a722533383fb2961ffe5585245374884c6d0c1a
admin admin <evaladmin@example.com>
2020-07-14 11:04:55 +0700

    User admin wrote content /static-assets/js/nav-link.js

--- static-assets/js/nav-link.js
+++ static-assets/js/nav-link.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var url = window.location;
-    
+    $('#nav-content a').removeClass('active');
     $('#nav-content a').filter(function() {
         return this.href == url;
     }).addClass('active');
