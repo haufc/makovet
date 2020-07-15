ea61e7684212066cb746de52e3304d86b856c775
admin admin <evaladmin@example.com>
2020-07-15 16:45:18 +0700

    User admin wrote content /scripts/components/techsupportcomponent.groovy

--- scripts/components/techsupportcomponent.groovy
+++ scripts/components/techsupportcomponent.groovy
@@ -3,5 +3,5 @@ import org.craftercms.blueprints.headless.SupportTechSearchHelper
 def searchHelper = new SupportTechSearchHelper(elasticsearch, urlTransformationService)
 def category = contentModel.supportTech_s
 
-def supportTech = searchHelper.searchSupportTech(category,0)
+def supportTech = searchHelper.searchSupportTech(category,0,3)
 templateModel.supportTech = supportTech
