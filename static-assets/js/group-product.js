d705aa10fc61685ef11915e1b270d98f68089b77
admin admin <evaladmin@example.com>
2020-07-16 10:36:33 +0700

    User admin wrote content /static-assets/js/group-product.js

--- static-assets/js/group-product.js
+++ static-assets/js/group-product.js
@@ -77,13 +77,13 @@ $(document).ready(function(){
     });
     
     
-    var lstProduct = $('.product-item');
-    console.log(lstProduct.length);
+    // var lstProduct = $('.product-item');
+    // console.log(lstProduct.length);
     
-    for(let i=0; i < lstProduct.length; i ++){
-        if(i % 5 != 0 || i == 0) {
-            $('#panigation-product .product-item:eq('+i+')').after('<div class="col-lg-1 col-md-1"></div>');
-        }
-    }
+    // for(let i=0; i < lstProduct.length; i ++){
+    //     if(i % 5 != 0 || i == 0) {
+    //         $('#panigation-product .product-item:eq('+i+')').after('<div class="col-lg-1 col-md-1"></div>');
+    //     }
+    // }
     
 });
