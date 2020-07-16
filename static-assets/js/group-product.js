f47ff7dc64f80f29ae92f87d7e5c53c9404f2c3b
admin admin <evaladmin@example.com>
2020-07-16 10:52:33 +0700

    User admin wrote content /static-assets/js/group-product.js

--- static-assets/js/group-product.js
+++ static-assets/js/group-product.js
@@ -82,12 +82,12 @@ $(document).ready(function(){
     
     for(let i=0; i < lstProduct.length; i ++){
         if (i == 0) {
-            $(lstProduct[i]).css('padding-right', '1%');
+            $(lstProduct[i]).css('padding-right', '3%');
         }
         if((i + 1) % 5 != 0) {
-            $(lstProduct[i]).css('padding-right', '1%');
+            $(lstProduct[i]).css('padding-right', '3%');
         } else {
-            $(lstProduct[i]).css('padding-left', '1%');
+            $(lstProduct[i]).css('padding-left', '3%');
         }
         
     }
