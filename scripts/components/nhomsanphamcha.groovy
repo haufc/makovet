991247093c8c2cbf64bebadf6c5fb49a0b4f33c0
admin admin <evaladmin@example.com>
2020-07-21 09:26:25 +0700

    User admin wrote content /scripts/components/nhomsanphamcha.groovy

--- scripts/components/nhomsanphamcha.groovy
+++ scripts/components/nhomsanphamcha.groovy
@@ -1,6 +1,7 @@
 import org.craftercms.blueprints.headless.GroupProductSearchHelper
 
 def searchHelper = new GroupProductSearchHelper(elasticsearch, urlTransformationService)
-def products = searchHelper.searchHotProducts(true,0)
-
+def products = searchHelper.searchHotProducts(true)
+print "size"
+print products.size()
 templateModel.products = products
