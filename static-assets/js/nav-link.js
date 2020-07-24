42fc29a9995c1337eeb6f9fc31502273da459cb2
admin admin <evaladmin@example.com>
2020-07-24 16:09:05 +0700

    User admin wrote content /static-assets/js/nav-link.js

--- static-assets/js/nav-link.js
+++ static-assets/js/nav-link.js
@@ -5,15 +5,23 @@ $(document).ready(function() {
         return this.href == url;
     }).addClass('active');
     
-    // if (url.href.indexOf('/gioi-thieu-chung') > -1) {
-    // $('#nav-content a').filter(function() {
-    //     return this.href == getContextPath()+"/gioithieu";
-    // }).addClass('active');
-    // }
+    if (url.href.indexOf('/gioi-thieu-chung') > -1) {
+    $('#nav-content a').filter(function() {
+        return this.href == getContextPath()+"/gioithieu";
+    }).addClass('active');
+    }
     
     if (url.href.indexOf('/san-pham-1') > -1) {
     $('#menu a').each(function() {
         if (this.href == getContextPath()+"/san-pham"){
+            $(this).addClass('active');
+        }
+    });
+    }
+    
+    if (url.href.indexOf('/ban-tin') > -1) {
+    $('#menu a').each(function() {
+        if (this.href == getContextPath()+"/tin-tuc"){
             $(this).addClass('active');
         }
     });
