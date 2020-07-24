2bd048a98468aa82b9dc201d35c25fc61b2a2eb2
admin admin <evaladmin@example.com>
2020-07-24 15:54:56 +0700

    User admin wrote content /static-assets/js/nav-link.js

--- static-assets/js/nav-link.js
+++ static-assets/js/nav-link.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
         return this.href == url;
     }).addClass('active');
     
-    if (url.href.indexOf('/gioi-thieu-chung') > -1) {
-    $('#nav-content a').filter(function() {
-        return this.href == getContextPath()+"/gioithieu";
-    }).addClass('active');
-    }
+    // if (url.href.indexOf('/gioi-thieu-chung') > -1) {
+    // $('#nav-content a').filter(function() {
+    //     return this.href == getContextPath()+"/gioithieu";
+    // }).addClass('active');
+    // }
     
     if (url.href.indexOf('/san-pham-1') > -1) {
     $('#nav-content a').filter(function() {
