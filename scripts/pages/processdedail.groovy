4f021d1bb9e50d6e92661cd153d2d21b97f1ae52
admin admin <evaladmin@example.com>
2020-07-29 09:26:36 +0700

    User admin wrote content /scripts/pages/processdedail.groovy

--- scripts/pages/processdedail.groovy
+++ scripts/pages/processdedail.groovy
@@ -1,7 +1,7 @@
 import org.craftercms.blueprints.headless.ProcessSearchHelper
 
 def searchHelper = new ProcessSearchHelper(elasticsearch, urlTransformationService)
-def category = contentModel.processType_o.key
+def category = contentModel.processType_o.item.key
 
 def processes = searchHelper.searchProcess(category.text,0)
 println "Processssssssssssssssssssssssssssssss"
