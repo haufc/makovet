a10a349291151e4c35e1844d80cd09d4e0317f2c
admin admin <evaladmin@example.com>
2020-07-31 14:23:43 +0700

    User admin wrote content /scripts/classes/org/craftercms/blueprints/headless/JobSearchHelper.groovy

--- scripts/classes/org/craftercms/blueprints/headless/JobSearchHelper.groovy
+++ scripts/classes/org/craftercms/blueprints/headless/JobSearchHelper.groovy
@@ -32,6 +32,7 @@ class JobSearchHelper {
             .query(QueryBuilders.queryStringQuery(q))
             .from(start)
             .size(rows)
+            .sort(new FieldSortBuilder("jobDuration_dt").order(SortOrder.DESC))
         
         def result = elasticsearch.search(new SearchRequest().source(builder))
         
@@ -52,7 +53,7 @@ class JobSearchHelper {
                 def job = [:]
                     job.title = doc.jobTitle_s
                     job.url = urlTransformationService.transform("storeUrlToRenderUrl", doc.localId)
-                    job.time = doc.jobDuration_s
+                    job.time = doc.jobDuration_dt
                 jobs << job
             }
         }
