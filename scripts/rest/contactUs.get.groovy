feae6de1d02caee365275bb1ded20c0d2edd3708
admin admin <evaladmin@example.com>
2020-08-03 11:36:46 +0700

    User admin wrote content /scripts/rest/contactUs.get.groovy

--- scripts/rest/contactUs.get.groovy
+++ scripts/rest/contactUs.get.groovy
@@ -9,7 +9,7 @@ service.setHost('smtp.gmail.com')
 service.setPort('587')
 service.setEncoding('utf-8')
 
-service.sendEmail(params.email, "keysoft.hotro@gmail.com", null, 
+service.sendEmail(params.email, "it.keysoft@gmail.com", null, 
                   params.title ,"<strong>Họ Tên: </strong>" + params.name + "<br/>" + "<strong>Email: </strong>" + params.email +"<br/>" + "<strong>Số điện thoại: </strong>" 
                   + params.phone + "<br/>" +"<strong>Tên công ty: </strong>" 
                   + params.company + "<br/>" + "<strong>Nội dung: </strong>" + params.content)
