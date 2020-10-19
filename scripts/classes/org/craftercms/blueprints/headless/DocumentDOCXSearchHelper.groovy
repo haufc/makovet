3d70876026ee1ab73401ca37c80b837d42685871
admin admin <evaladmin@example.com>
2020-10-19 15:20:04 +0700

    User admin wrote content /scripts/classes/org/craftercms/blueprints/headless/DocumentDOCXSearchHelper.groovy

--- scripts/classes/org/craftercms/blueprints/headless/DocumentDOCXSearchHelper.groovy
+++ scripts/classes/org/craftercms/blueprints/headless/DocumentDOCXSearchHelper.groovy
@@ -57,7 +57,7 @@ class DocumentDOCXSearchHelper {
                 def item = [:]
                     item.title = doc.title_s
                     item.url = urlTransformationService.transform("storeUrlToRenderUrl", doc.localId)
-                    item.urldown = doc.dOCXfile_o.item.key
+                    item.urldown = doc.dOCXfile_o.item
                     item.avatar = doc.avatar_s
                 docs << item
             }
